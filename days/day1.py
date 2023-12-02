f80cbc4c632b88b7e140343c3cce14a38b722d0d
Paul (Mac) <paul@nettorrent.com>
2023-12-02 14:35:20 +0000

    Day 1 Part 2

--- days/day1.py
+++ days/day1.py
@@ -26,7 +26,44 @@ def part1(data: list[str]) -> int:
 
 def part2(data: list[str]) -> int:
 
-    return 281
+    accum = 0
+    char_value = "0"
+    for data_line in data:
+        if data_line == "":
+            pass
+        else:
+            char_value = ""
+            res_list = re.findall(r"[\d]|[one]+|[two]+|[three]+|[four]+|[five]+|[six]+|[seven]+|[eight]+|[nine]+", data_line)
+            if res_list:
+                character = res_list[0].index.group()
+                if character.isnumeric() : 
+                    char_value += character
+                else:
+                    match character :
+                        case "one" : 
+                                character = "1"
+                        case "two" : 
+                                character = "2"
+                        case "three" : 
+                                character = "3"
+                        case "four" : 
+                                character = "4"
+                        case "five" : 
+                                character = "5"
+                        case "six" : 
+                                character = "6"
+                        case "seven" : 
+                                character = "7"
+                        case "eight" : 
+                                character = "8"
+                        case "nine" : 
+                                character = "9"
+                                
+                    char_value += character
+                
+        accum += int(char_value)
+
+    return accum
 
 
 
