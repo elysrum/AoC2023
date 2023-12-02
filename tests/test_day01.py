e48a2ac4e845bb1d6f13621db2b5e0586103c835
Paul (Mac) <paul@nettorrent.com>
2023-12-02 12:38:52 +0000

    Day 1 - prep work

--- tests/test_day01.py
+++ tests/test_day01.py
@@ -3,10 +3,13 @@ import days.day1
 
 class TestDay01(unittest.TestCase):
 
-    data = ["1000","2000","3000","\n","4000","\n","5000","6000","\n","7000","8000","9000","\n","10000\n", "\n"]
+    data = ["1abc2\n",
+"pqr3stu8vwx\n",
+"a1b2c3d4e5f\n",
+"treb7uchet\n"]
 
     def test_day01_part1(self):
-        self.assertEqual(days.day1.part1(self.data), 24000)
+        self.assertEqual(days.day1.part1(self.data), 142)
     
     def test_day01_part2(self):
         self.assertEqual(days.day1.part2(self.data), 45000)
