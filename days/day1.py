26d1e298d62ad12542b1f0e886eea4498d85988b
Paul (Mac) <paul@nettorrent.com>
2023-12-02 13:14:10 +0000

    day 1 part 1 complete

--- days/day1.py
+++ days/day1.py
@@ -2,24 +2,28 @@ import support
 
 def part1(data: list[str]) -> int:
 
-    sorted_elfs = sumupelves(data)
-    return sorted_elfs[-1]
+    accum = 0
+    for data_line in data:
+        if data_line == "":
+            pass
+        else:
+            char_value = ""
+            for index, character in enumerate(data_line):
+                if character.isdigit():
+                    char_value += character
+                    break
+            for index, character in enumerate(reversed(data_line)):
+                if character.isdigit():
+                    char_value += character
+                    break
+
+        accum += int(char_value)
+
+    return accum
 
 def part2(data: list[str]) -> int:
 
-    sorted_elfs = sumupelves(data)
-    return sorted_elfs[-1] + sorted_elfs[-2] + sorted_elfs[-3]
+    return True
 
-def sumupelves(data: list[str]):
-    elf_cal = 0
-    elfs = []
 
-    for calories in data:
-        if calories == '\n':
-            elfs.append(elf_cal)
-            elf_cal = 0
-        else:
-            elf_cal = elf_cal + int(calories)
-
-    return sorted(elfs)
 
