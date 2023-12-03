6f1066707601dd6aeabb8e8c4bda3f45f0110507
Paul Carr <github@nettorrent.com>
2023-12-03 15:55:35 +0000

    Forgot to commit part 1.  Parts 1&2 Complete. REALLY UGLY SOLUTION, Needs refactoring badly.

--- days/day3.py
+++ days/day3.py
@@ -1,43 +1,170 @@
+import re
 def part1(data: list[str]) -> int:
-    score = 0
-    lowScoreDict = "abcdefghijklmnopqrstuvwxyz"
-    highscoreDict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    parts_list = []
+    parts_sum = 0
 
-    for sack in data:
-        s=sack.strip()
-        mid = round(len(s)/2)
+    # For every line in the data set
+    parts_in_line = []
+    prev_parts_in_line = []
+    symbol_locn = []
+    prev_symbol_locn = []
+
+    for line in data:
+
+        part_number = ""
+        start_found = False
+        end_found = False
+        start_loc = 0
+        part_length = 0
+
+        # Extract every numnber on the line with start and length
+        for index, char in enumerate(line.strip()) :
+            
+            # Is the character a number, yes build the number string
+            if char.isnumeric() :
+                part_number += char
+                part_length += 1
+                if not start_found : 
+                    start_loc = index
+                    start_found = True
+            elif char == '.':
+                if start_found :
+                    end_found = True
+            else :
+                symbol_locn.append((char, index))
+                if start_found :
+                    end_found = True
+           
+            if start_found and end_found :
+                start_found = False
+                end_found = False
+                parts_in_line.append((part_number, start_loc, part_length))
+                part_number = ""
+                start_loc = 0
+                part_length = 0
         
-        comp1 = set(s[:mid])
-        comp2 = set(s[mid:])
+        #Need to cater for the case where we run out of string
+        if start_found and not end_found :
+            parts_in_line.append((part_number, start_loc, part_length))
 
-        dupItem = (comp1 & comp2).pop()
+        # Match Current Line Numbers against Current Line Symbols
 
-        x = lowScoreDict.find(dupItem)
-        if x == -1:
-            x = highscoreDict.find(dupItem)
-            x += 26
-        
-        score += x + 1
+        for (part_number, start_loc, part_length) in parts_in_line[:] :
+            for (symbol, symbol_index) in symbol_locn :
+                if (symbol_index >= start_loc - 1) and (symbol_index <= start_loc + part_length) : 
+                    parts_list.append(part_number)
+                    parts_in_line.remove((part_number, start_loc, part_length))
 
-    return score
+            # Match against previous line symbols
+        for (part_number, start_loc, part_length) in parts_in_line[:] :
+            for (symbol, symbol_index) in prev_symbol_locn :
+                if (symbol_index >= start_loc - 1) and (symbol_index <= start_loc + part_length) : 
+                    parts_list.append(part_number)
+                    parts_in_line.remove((part_number, start_loc, part_length))
+
+        #Match previous numbers agains current symbols
+        for (part_number, start_loc, part_length) in prev_parts_in_line :
+            for (symbol, symbol_index) in symbol_locn :
+                if (symbol_index >= start_loc - 1) and (symbol_index <= start_loc + part_length) : 
+                    parts_list.append(part_number)
+
+
+
+        # Copy Current Symbols to Previous Symbols
+        prev_symbol_locn = symbol_locn.copy()
+        prev_parts_in_line = parts_in_line.copy()
+        parts_in_line = []
+        symbol_locn = []
+
+    for part in parts_list :
+        parts_sum += int(part)
+
+    return parts_sum
 
 def part2(data: list[str]) -> int:
-    score = 0
-    lowScoreDict = "abcdefghijklmnopqrstuvwxyz"
-    highscoreDict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    parts_list = []
+    parts_sum = 0
 
-    for i in range(0, len(data), 3):
-        sack1=set(data[i].strip())
-        sack2=set(data[i+1].strip())
-        sack3=set(data[i+2].strip())
+    # For every line in the data set
+    parts_in_line = []
+    prev_parts_in_line = []
+    symbol_locn = []
+    prev_symbol_locn = []
 
-        dupItem = (sack1 & sack2 & sack3).pop()
+    line_number = 0
+    for line in data:
 
-        x = lowScoreDict.find(dupItem)
-        if x == -1:
-            x = highscoreDict.find(dupItem)
-            x += 26
+        part_number = ""
+        start_found = False
+        end_found = False
+        start_loc = 0
+        part_length = 0
+
+        # Extract every numnber on the line with start and length
+        for index, char in enumerate(line.strip()) :
+            
+            # Is the character a number, yes build the number string
+            if char.isnumeric() :
+                part_number += char
+                part_length += 1
+                if not start_found : 
+                    start_loc = index
+                    start_found = True
+            elif char == '.':
+                if start_found :
+                    end_found = True
+            else :
+                if start_found :
+                    end_found = True
+                if char == '*' :
+                    symbol_locn.append((char, index))
+            
+            if start_found and end_found :
+                start_found = False
+                end_found = False
+                parts_in_line.append((part_number, start_loc, part_length))
+                part_number = ""
+                start_loc = 0
+                part_length = 0
         
-        score += x + 1 # because find is 0 based
+        #Need to cater for the case where we run out of string
+        if start_found and not end_found :
+            parts_in_line.append((part_number, start_loc, part_length))
 
-    return score
+        # Match Current Line Numbers against Current Line Symbols
+
+        for (part_number, start_loc, part_length) in parts_in_line[:] :
+            for (symbol, symbol_index) in symbol_locn :
+                if (symbol_index >= start_loc - 1) and (symbol_index <= start_loc + part_length) : 
+                    parts_list.append((line_number, symbol_index, part_number))
+                    parts_in_line.remove((part_number, start_loc, part_length))
+
+            # Match against previous line symbols
+        for (part_number, start_loc, part_length) in parts_in_line[:] :
+            for (symbol, symbol_index) in prev_symbol_locn :
+                if (symbol_index >= start_loc - 1) and (symbol_index <= start_loc + part_length) : 
+                    parts_list.append((line_number, symbol_index, part_number))
+                    parts_in_line.remove((part_number, start_loc, part_length))
+
+        #Match previous numbers agains current symbols
+        for (part_number, start_loc, part_length) in prev_parts_in_line :
+            for (symbol, symbol_index) in symbol_locn :
+                if (symbol_index >= start_loc - 1) and (symbol_index <= start_loc + part_length) : 
+                    parts_list.append((line_number, symbol_index, part_number))
+
+
+
+        # Copy Current Symbols to Previous Symbols
+        prev_symbol_locn = symbol_locn.copy()
+        prev_parts_in_line = parts_in_line.copy()
+        parts_in_line = []
+        symbol_locn = []
+        line_number += 1
+    
+    for (line_number1, index1, part1) in parts_list[:] :
+        parts_list.remove((line_number1, index1, part1))
+        for (line_number2, index2, part2) in parts_list :
+            if index1 == index2 and (line_number1 == line_number2 or line_number1 == line_number2 - 1) :
+                parts_sum += int(part1) * int(part2)
+
+    return parts_sum
--- tests/test_day03.py
+++ tests/test_day03.py
@@ -3,15 +3,48 @@ import days.day3
 
 class TestDay03(unittest.TestCase):
 
-    data = ["vJrwpWtwJgWrhcsFMMfFFhFp",
-"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-"PmmdzqPrVvPwwTWBwg",
-"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-"ttgJtRGJQctTZtZT",
-"CrZsJsPPZsGzwwsLwLmpwMDw]"]
+    data = ["467..114..\n",
+            "...*......\n",
+            "..35..633.\n",
+            "......#...\n",
+            "617*......\n",
+            ".....+.58.\n",
+            "..592.....\n",
+            "......755.\n",
+            "...$.*....\n",
+            ".664.598..\n"
+]
+    data1 = [
+        "..10.10...\n",
+        "....*.....\n",   # Check Consecutive Matches
+        ".......100\n",   # No Match
+        "...10*10..\n",  # Check Consecutive matches
+        "..10......\n",  # No Match 
+        "....*.....\n",  #  No Match
+        ".....10...\n",
+        "....*.....\n",  #  Check Double Match
+        "100.......\n",
+        "*........*\n",
+        ".......100\n"
+]
+
+    data2 = [
+        ".100.100..\n",
+        "..100*100.\n",  # Check Consecutive matches
+        "....*.....\n",  #  Check Double Match
+        ".100.100..\n",   # Check Consecutive Matches
+        "....*.....\n",  #  Check Double Match
+        ".100.100..\n",
+        "100.......\n",
+        "@........@\n",
+        ".......100\n"
+]
 
     def test_day03_part1(self):
-        self.assertEqual(days.day3.part1(self.data), 157)
+        self.assertEqual(days.day3.part1(self.data), 4361)
+        self.assertEqual(days.day3.part1(self.data2), 900)
     
     def test_day03_part2(self):
-        self.assertEqual(days.day3.part2(self.data), 70)
+        self.assertEqual(days.day3.part2(self.data1), 300)
+
+        self.assertEqual(days.day3.part2(self.data), 467835)
