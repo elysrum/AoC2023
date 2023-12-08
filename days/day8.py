118c4f9de52d1169035c0d008de052fd7e0c43e8
Paul Carr <github@nettorrent.com>
2023-12-08 09:55:23 +0000

    Day 8 Part 1 Complete

--- days/day8.py
+++ days/day8.py
@@ -2,8 +2,38 @@ from collections import defaultdict
 
 def part1(data: list[str]) -> int:
 
+    data_set = ("".join(data))
+    posn = {}
+        
 
-    return 2
+    instructions, location_set = data_set.strip().split("\n\n")
+    locations = location_set.strip().split("\n")
+
+    for locn in locations:
+        pos = locn[:3]
+        n1 = locn[7:10]
+        n2 = locn[12:15]
+        posn[pos] = (n1, n2)
+
+    exitFound = False
+    i = 0
+
+    next_dest = "AAA"
+
+    while not exitFound:
+        lookup = (i % len(instructions))
+        i += 1
+        curr_pos = posn[next_dest]
+
+        if (instructions[lookup] == "R") :
+            next_dest = curr_pos[1]
+        else:
+            next_dest = curr_pos[0]
+
+        if next_dest == "ZZZ" :
+            exitFound = True
+
+    return i
 
 def part2(data: list[str]) -> int:
 
--- tests/test_day07.py
+++ tests/test_day07.py
@@ -5,20 +5,14 @@ class TestDay07(unittest.TestCase):
 
     data = ["32T3K 765\n",
 "T55J5 684\n",
-"JJJJJ 10\n",
-"J425J 12\n",
 "KK677 28\n",
-"24JK6 53\n",
-"22222 20\n",
 "KTJJT 220\n",
 "QQQJA 483\n",
-"22643 482\n",
-"AQJ49 400"
 ]
 
     def test_day07_part1(self):
         self.assertEqual(days.day7.part1(self.data), 6440)
 
     def test_day07_part2(self):
-        self.assertEqual(days.day7.part2(self.data), 6205)
+        self.assertEqual(days.day7.part2(self.data), 5905)
 
