d3ba1d947d7a2ec55f487dddd992aa9c09cb6369
Paul Carr <github@nettorrent.com>
2023-12-08 10:38:28 +0000

    Not Working

--- days/day8.py
+++ days/day8.py
@@ -37,6 +37,43 @@ def part1(data: list[str]) -> int:
 
 def part2(data: list[str]) -> int:
 
+    data_set = ("".join(data))
+    posn = {}
+    start_nodes = []
+        
 
-    return 8
+    instructions, location_set = data_set.strip().split("\n\n")
+    locations = location_set.strip().split("\n")
+
+    for locn in locations:
+        pos = locn[:3]
+        n1 = locn[7:10]
+        n2 = locn[12:15]
+        posn[pos] = (n1, n2)
+        if pos[2] == "A" :
+            start_nodes.append(pos)
+
+    starting_count = len(start_nodes)
+    exitFound = False
+    i = 0
+
+    while not exitFound:
+        lookup = (i % len(instructions))
+        i += 1
+        new_nodes = []
+        for node in start_nodes:
+            curr_pos = posn[node]
+
+            if (instructions[lookup] == "R") :
+                new_nodes.append(curr_pos[1])
+            else:
+                new_nodes.append(curr_pos[0])
+
+        count = len(list((filter(lambda x: x[2] == "Z", new_nodes))))
+        if count == starting_count:
+             exitFound = True
+        
+        start_nodes = new_nodes
+
+    return i
 
--- tests/test_day08.py
+++ tests/test_day08.py
@@ -13,10 +13,21 @@ class TestDay08(unittest.TestCase):
 "GGG = (GGG, GGG)\n",
 "ZZZ = (ZZZ, ZZZ)\n"
 ]
-
+    
+    data2 = ["LR\n",
+"\n",
+"11A = (11B, XXX)\n",
+"11B = (XXX, 11Z)\n",
+"11Z = (11B, XXX)\n",
+"22A = (22B, XXX)\n",
+"22B = (22C, 22C)\n",
+"22C = (22Z, 22Z)\n",
+"22Z = (22B, 22B)\n",
+"XXX = (XXX, XXX)\n"
+    ]
     def test_day08_part1(self):
         self.assertEqual(days.day8.part1(self.data), 2)
 
     def test_day08_part2(self):
-        self.assertEqual(days.day8.part2(self.data), 8)
+        self.assertEqual(days.day8.part2(self.data2), 6)
 
