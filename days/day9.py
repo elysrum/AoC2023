a30636cebcccf2245ff99cd796a062a0162966b3
Paul Carr <github@nettorrent.com>
2023-12-09 13:45:44 +0000

    Day 9  Part 1 & 2 Complete

--- days/day9.py
+++ days/day9.py
@@ -1,106 +1,62 @@
-from collections import defaultdict
+from functools import reduce
+import operator
+
+# Are all elements in the list the same?
+def all_same(lst):
+    if len(lst) < 0:
+        res = True
+    res = all(ele == lst[0] for ele in lst)
+    return res
+
+def part1_recurse(num_list: list[int]) -> list[int]:
+    new_list = []
+    for i in range(1, len(num_list)):
+        new_list.append(num_list[i] - num_list[i-1])
+
+    if all_same(new_list) :
+        pass
+    else:
+        part1_recurse(new_list)
+    
+    num_list.append(num_list[len(num_list) - 1] + int(new_list[len(new_list) - 1]))
+    return num_list
 
 def part1(data: list[str]) -> int:
+    number_list = []
 
+    retVal = 0
 
-    Head = (0, 0)
-    Tail = (0, 0)
+    for line in data :
+        number_list = [int(x) for x in line.strip().split()]
+        new_list = part1_recurse(number_list)
+        retVal += new_list[len(new_list) - 1]
 
-    Tail_loc = {Tail}
+    return retVal
 
-    for instruction in data :
-        direction, distance = instruction.strip().split()
+def part2_recurse(num_list: list[int]) -> list[int]:
+    new_list = []
+    for i in range(1, len(num_list)):
+        new_list.append(num_list[i] - num_list[i-1])
+
+    if all_same(new_list) :
+        pass
+    else:
+        part2_recurse(new_list)
     
-        distance = int(distance)
-        for i in range(distance) :
-
-            head0 = Head[0]
-            head1 = Head[1]
-            if direction == "L" :
-                head0 -= 1
-            elif direction == "R" :
-                head0 += 1
-            elif direction == "U" :
-                head1 += 1
-            elif direction == "D" :
-                head1 -= 1
-            else : 
-                pass
-
-            Head = (head0, head1)
-            Tail = move_tail(Head, Tail)
-            Tail_loc.add(Tail)
-
-    return len(Tail_loc)
+    num_list.insert(0, num_list[0] - new_list[0])
+    return num_list
 
 
 def part2(data: list[str]) -> int:
 
-    Head = (0, 0)
+    number_list = []
 
-    Rope = [(0,0) for _ in range(9)]
-    Tail_loc = set([Rope[8]])
+    retVal = 0
 
-    for instruction in data :
-        direction, distance = instruction.strip().split()
-    
-        distance = int(distance)
-        for _ in range(distance) :
+    for line in data :
+        number_list = [int(x) for x in line.strip().split()]
+        new_list = part2_recurse(number_list)
+        retVal += new_list[0]
 
-            head0 = Head[0]
-            head1 = Head[1]
-            if direction == "L" :
-                head0 -= 1
-            elif direction == "R" :
-                head0 += 1
-            elif direction == "U" :
-                head1 += 1
-            elif direction == "D" :
-                head1 -= 1
-            else : 
-                pass
+    return retVal
 
-            Head = (head0, head1)
-
-            Rope[0] = move_tail(Head, Rope[0])
-            for i in range(1, 9) :
-                Rope[i] = move_tail(Rope[i-1], Rope[i])
-
-            Tail_loc.add(Rope[8])
-
-    return len(Tail_loc)
-
-def move_tail(Head: tuple, Tail: tuple) -> tuple :
-
-
-    tail0 = Tail[0]
-    tail1 = Tail[1]
-
-    if (Head[0] - Tail[0]) > 1  and (Head[1] - Tail[1]) == 0:
-        tail0 += 1
-    elif (Head[0] - Tail[0]) < -1 and (Head[1] - Tail[1]) == 0 :
-        tail0 -= 1
-    elif (Head[0] - Tail[0]) == 0 and (Head[1] - Tail[1]) > 1 :
-        tail1 += 1
-    elif (Head[0] - Tail[0]) == 0 and (Head[1] -  Tail[1]) < -1 :
-        tail1 -= 1
-    elif (((Head[0] - Tail[0]) > 1 and (Head[1] - Tail[1]) >= 1) or
-          ((Head[0] - Tail[0]) >= 1 and (Head[1] - Tail[1]) > 1)) :
-        tail0 += 1
-        tail1 += 1
-    elif (((Head[0] - Tail[0]) < -1 and (Head[1] - Tail[1]) >= 1) or 
-          ((Head[0] - Tail[0]) <= -1 and (Head[1] - Tail[1]) > 1)) :
-        tail0 -= 1
-        tail1 += 1
-    elif (((Head[0] - Tail[0]) > 1 and (Head[1] - Tail[1]) <= -1) or 
-          ((Head[0] - Tail[0]) >= 1 and (Head[1] - Tail[1]) < -1 )) :
-        tail0 += 1
-        tail1 -= 1
-    elif (((Head[0] - Tail[0]) < -1 and (Head[1] - Tail[1]) <= -1) or 
-          ((Head[0] - Tail[0]) <= -1 and (Head[1] - Tail[1]) < -1)) :
-        tail0 -= 1
-        tail1 -= 1
-    else :   # if Head[0] == Tail[0] and Head[1] == Tail[1] 
-        pass
-
-    return (tail0,tail1)
--- tests/test_day09.py
+++ tests/test_day09.py
@@ -3,30 +3,14 @@ import days.day9
 
 class TestDay09(unittest.TestCase):
 
-    data = ["R 4\n",
-"U 4\n",
-"L 3\n",
-"D 1\n",
-"R 4\n",
-"D 1\n",
-"L 5\n",
-"R 2\n",
+    data = ["0 3 6 9 12 15\n",
+"1 3 6 10 15 21\n",
+"10 13 16 21 30 45\n"
 ]
 
     def test_day09_part1(self):
-        self.assertEqual(days.day9.part1(self.data), 13)
+        self.assertEqual(days.day9.part1(self.data), 114)
 
     def test_day09_part2(self):
-
-        self.data = ["R 5\n",
-"U 8\n",
-"L 8\n",
-"D 3\n",
-"R 17\n",
-"D 10\n",
-"L 25\n",
-"U 20\n"
-]
-
-        self.assertEqual(days.day9.part2(self.data), 36)
+        self.assertEqual(days.day9.part2(self.data), 2)
 
