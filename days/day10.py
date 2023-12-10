b02dcc840c046afbdac8c6a353c73a535a64cf45
Paul Carr <github@nettorrent.com>
2023-12-10 11:20:59 +0000

    Day 10 Prep

--- days/day10.py
+++ days/day10.py
@@ -1,66 +1,28 @@
 
 def part1(data: list[str]) -> int:
 
-    x = 1
-    cycle = 1
-    cycle_interval = 20
-    cycle_ceiling = 240
-    total = 0
+    pipes = [][]
+    start_coord = (0,0)
 
-    for command_line in data :
-        commands = command_line.strip().split()
-        for op in commands :
-            if op == "noop" :
-                pass
-            elif op == "addx" : 
-                pass
-            else :
-                x = x + int(op)
-            cycle += 1
-            if cycle == cycle_interval and cycle <= cycle_ceiling:
-                total += (x *  cycle)
-                cycle_interval += 40
+    y = 0
+    for line in data:
+        x = 0
+        for pipe in line.strip():
+            pipes[x][y] = pipe
+            y = y + 1
+
+            if pipe == "S":
+                start_coord = (x,y)
+
+        x = x + 1
+
+    print(start_coord)
 
 
-    return total
+    return 4
 
 
-def part2(data: list[str]) -> str:
-
-    x = 1
-    cycle = 1
-    screen_width = 40
-    cycle_ceiling = 240
-    screen_row = 0
-    total = 0
-    screen = [["." for _ in range(40)] for _ in range(6)]
+def part2(data: list[str]) -> int:
 
 
-    xpos = cycle
-
-    for command_line in data :
-        commands = command_line.strip().split()
-        for op in commands :
-
-            # Is sprite visible
-            if x == xpos or x+1 == xpos or (x+2) == xpos :
-                screen[screen_row][xpos-1] = "#"
-
-            if op == "noop" :
-                pass
-            elif op == "addx" : 
-                pass
-            else :
-                x += int(op)
-                pass
-
-            cycle += 1
-            screen_row, xpos = divmod(cycle, screen_width)
-            if (xpos == 0 ) : 
-                screen_row -= 1
-                xpos = 40
-
-
-    ret_val = str("".join(screen[0][:])) + str("".join(screen[1][:])) + str("".join(screen[2][:])) + str("".join(screen[3][:])) + str("".join(screen[4][:])) + str("".join(screen[5][:]))
-
-    return ret_val
+    return 10
