2a4345e13cc2ddb157e2a48b0c25d7bd5c8da05a
Paul Carr <github@nettorrent.com>
2023-12-11 20:53:45 +0000

    Day 11 Part 2 complete and refactored a bit

--- days/day11.py
+++ days/day11.py
@@ -1,28 +1,36 @@
 from collections import Counter
 
-def expand_universe(data: list[list], expanse: int) -> list[list]:
+EmptyRows = []
+EmptyColumns = []
+
+def expand_universe(data: list[list]) -> list[list]:
 
     new_universe = []
+
+    # Clean out empty rows and columns - important if this is part2 running after part1
+    EmptyRows.clear()
+    EmptyColumns.clear()
+
     # Expand the number of rows first - its the easier of the two
     for row in range(len(data)):
         line = data[row]
         # Is the whole line "."
         new_universe.append(line)
         if all(char == "." for char in line) :
-            for _ in range(0, expanse - 1):
-                new_universe.append(line[:]) # make sure we append a copy otherwise we reference same thing
+            EmptyRows.append(row)
+
+
 
     offset = 0
     for col in range(len(new_universe[0])):
         count = Counter()
         for row in range(len(new_universe)):
             count[new_universe[row][col + offset]] += 1
-        if count["."] == len(new_universe) :
+
+        if (count["."] + count["o"]) == len(new_universe) :
             # Entire Column is "."
-            for nrow in range(len(new_universe)):
-                for o in range (0, expanse - 1):
-                    new_universe[nrow].insert(col + offset + o, ".")
-            offset += expanse - 1
+            EmptyColumns.append(col)
+
 
     return new_universe
 
@@ -40,7 +48,7 @@ def part1(data: list[str]) -> int:
         y = y + 1
 
     # First Expand the Universe
-    new_universe = expand_universe(universe, 1)
+    new_universe = expand_universe(universe)
 
     ROWS = len(new_universe)
     COLS = len(new_universe[0])
@@ -56,16 +64,29 @@ def part1(data: list[str]) -> int:
 
     sumDist = 0
 
-    for i in range(0, num_galaxies):
-        (x, y) = galaxies[i]
-        for j in range(i, num_galaxies):
-            (dx, dy) = galaxies[j]
-            sumDist += abs(x - dx) + abs(y - dy)
-            
+    empty_cost = 1
 
+    for i in range(num_galaxies):
+        y, x = galaxies[i]
+        for j in range(i + 1, num_galaxies):
+            dy, dx = galaxies[j]
+            
+            steps = abs(x - dx) + abs(y - dy)
+
+            for empty_row in EmptyRows:
+                if min(y, dy) <= empty_row <= max(y, dy):
+                    steps += empty_cost
+            for empty_col in EmptyColumns:
+                if min(x, dx) <= empty_col <= max(x, dx):
+                    steps += empty_cost
+            # Algo here
+            sumDist += steps
+
+            
     return sumDist
 
 def part2(data: list[str]) -> int:
+
     # INgest the universe
     universe = [["." for i in range(len(data[0]) - 1)] for i in range(len(data))]
     # Build the map in memory
@@ -78,7 +99,7 @@ def part2(data: list[str]) -> int:
         y = y + 1
 
     # First Expand the Universe
-    new_universe = expand_universe(universe, 1000000)
+    new_universe = expand_universe(universe)
 
     ROWS = len(new_universe)
     COLS = len(new_universe[0])
@@ -94,11 +115,22 @@ def part2(data: list[str]) -> int:
 
     sumDist = 0
 
-    for i in range(0, num_galaxies):
-        (x, y) = galaxies[i]
-        for j in range(i, num_galaxies):
-            (dx, dy) = galaxies[j]
-            sumDist += abs(x - dx) + abs(y - dy)
+    empty_cost = 10**6 - 1
+
+    for i in range(num_galaxies):
+        y, x = galaxies[i]
+        for j in range(i + 1, num_galaxies):
+            dy, dx = galaxies[j]
             
+            steps = abs(x - dx) + abs(y - dy)
+
+            for empty_row in EmptyRows:
+                if min(y, dy) <= empty_row <= max(y, dy):
+                    steps += empty_cost
+            for empty_col in EmptyColumns:
+                if min(x, dx) <= empty_col <= max(x, dx):
+                    steps += empty_cost
+            # Algo here
+            sumDist += steps
 
     return sumDist
--- tests/test_day11.py
+++ tests/test_day11.py
@@ -19,5 +19,5 @@ class TestDay11(unittest.TestCase):
         self.assertEqual(days.day11.part1(self.data), 374)
 
     def test_day11_part2(self):
-        self.assertEqual(days.day11.part2(self.data), 8410)
+        self.assertEqual(days.day11.part2(self.data), 82000210)
 
